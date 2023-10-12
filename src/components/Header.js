a8ecb105cee6c95ebce26681e5cd64ec75a662c0
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2023-10-12 16:32:16 +0200

    [US-011] updated the element of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <>
       <header className="header-upper py-3">
         <div className="container-xxl">
-          <div className="row" align-items-center>
+          <div className="row align-items-center">
             <div className="col-2">
               <h1>
                 <Link className="header-upper-logo">
@@ -15,35 +15,72 @@ const Header = () => {
                 </Link>
               </h1>
             </div>
-            <div className="col-5">
+            <div className="col-3">
               <div className="input-group">
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
                 <input
                   type="text"
                   class="form-control py-2"
-                  placeholder="Chercher dans la boutique"
-                  aria-label="Chercher dans la boutique"
+                  placeholder="Rechercher un produit..."
+                  aria-label="Rechercher un produit..."
                   aria-describedby="basic-addon2"
-                  />
-                <span className="input-group-text p-3" id="basic-addon2">
-                  <BsSearch className='fs-6'/>
-                </span>
+                />
               </div>
-            </div> 
-            <div className="col-3">
-              <div className="header-upper-links d-flex align-items-center justify-content-between">
+            </div>
+            <div className="col-7">
+              <div className="header-upper-links d-flex align-items-center justify-content-end text-dark gap-60">
+                <div className="menu-links d-flex align-items-center">
+                  <div className="dropdown">
+                    <button
+                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 d-flex gap-10 align-items-center"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      <span>Menu</span>
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                      <li>
+                        <Link className="dropdown-item" to="">
+                          Action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" to="">
+                          Another action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" to="">
+                          Something else here
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
                 <div>
                   <Link className="d-flex align-items-center gap-10 text-dark">
-                    <img src="img/account.svg" alt="Mon compte" />
-                    <p className="mb-0">Se connecter <br/>à mon compte</p>
+                    <p className="mb-0">À propos</p>
                   </Link>
                 </div>
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-dark" >
-                  <img src="img/cart.svg" alt="Panier" />
-                  <div className="d-flex flex-column">
-                    <span className="badge bg-dark text-white">0</span>
-                    <p className="mb-0">50 €</p>
-                  </div>
+                  <Link className="d-flex align-items-center gap-10 text-dark">
+                    <p className="mb-0">Contact</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-dark">
+                    <p className="mb-0">Compte</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-dark">
+                    <img src="img/cart.svg" alt="Panier" />
+                    <div className="d-flex flex-column">
+                      <span className="badge bg-dark text-white">0</span>
+                      <p className="mb-0">50 €</p>
+                    </div>
                   </Link>
                 </div>
               </div>
@@ -52,7 +89,7 @@ const Header = () => {
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
