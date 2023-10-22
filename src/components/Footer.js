e51303c1cdc445ccef7002782d16cf36b71a988c
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2023-10-22 17:44:49 +0200

    [US-023] responsive of header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,29 +8,29 @@ const Footer = () => {
     <footer className="py-3">
       <div className="container-xxl">
         <div className="row">
-          <div className="col-2">
+          <div className="col-lg-3 col-md-3">
             <Link className="footer-upper-logo ">
               <img src="img/logo.svg" alt="Logo Kido'z" />
             </Link>
           </div>
-          <div className="col-3">
-            <h4 className="mb-4">À propos</h4>
+          <div className="col-lg-3 col-md-3">
+            <h4>À propos</h4>
             <div className="footer-link d-flex flex-column">
               <Link className="text-dark py-2 mb-1">Notre engagement</Link>
               <Link className="text-dark py-2 mb-1">Qui sommes-nous ?</Link>
               <Link className="text-dark py-2 mb-1">Paiement sécurisé</Link>
             </div>
           </div>
-          <div className="col-3">
-            <h4 className="mb-4">Besoin d'aide</h4>
+          <div className="col-lg-3 col-md-3">
+            <h4>Besoin d'aide</h4>
             <div className="footer-link d-flex flex-column">
               <Link className="text-dark py-2 mb-1">FAQ</Link>
               <Link className="text-dark py-2 mb-1">Contact</Link>
               <Link className="text-dark py-2 mb-1">Pages légales</Link>
             </div>
           </div>
-          <div className="col-3">
-            <h4 className="mb-4">Nos réseaux sociaux</h4>
+          <div className="col-lg-3 col-md-3">
+            <h4>Nos réseaux sociaux</h4>
             <div>
               <div className="social-icons d-flex align-items-item-center gap-30">
                 <a href="">
@@ -56,7 +56,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      </footer>  
+    </footer>  
     </>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import { BsSearch } from 'react-icons/bs';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+import { LuMenu } from "react-icons/lu";
 
 const Header = () => {
   return (
     <>
-      <header className="header-upper py-3">
+      {/* <header className="header py-3">
         <div className="container-xxl">
-          <div className="row align-items-center">
+          <div className=" d-flex align-items-center">
             <div className="col-2">
               <h1>
-                <Link className="header-upper-logo">
+                <Link className="header-logo">
                   <img src="img/logo.svg" alt="Logo Kido'z" />
                 </Link>
               </h1>
@@ -22,7 +23,7 @@ const Header = () => {
                 </span>
                 <input
                   type="text"
-                  class="form-control py-2"
+                  className="form-control py-2"
                   placeholder="Rechercher un produit..."
                   aria-label="Rechercher un produit..."
                   aria-describedby="basic-addon2"
@@ -30,64 +31,148 @@ const Header = () => {
               </div>
             </div>
             <div className="col-7">
-              <div className="header-upper-links d-flex align-items-center justify-content-end text-dark gap-60">
-                <div className="menu-links d-flex align-items-center">
-                  <div className="dropdown">
-                    <button
-                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 d-flex gap-10 align-items-center"
-                      type="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false">
-                      <span>Menu</span>
-                    </button>
-                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                      <li>
-                        <Link className="dropdown-item" to="">
-                          Action
-                        </Link>
-                      </li>
-                      <li>
-                        <Link className="dropdown-item" to="">
-                          Another action
-                        </Link>
-                      </li>
-                      <li>
-                        <Link className="dropdown-item" to="">
-                          Something else here
-                        </Link>
-                      </li>
+              <div className="header-links d-flex align-items-center">
+                
+                <ul className="navigation d-flex align-items-center list-unstyled mb-0 gap-60">
+                  <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Menu
+                    </a>
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a className="dropdown-item" href="#">Action</a></li>
+                      <li><a className="dropdown-item" href="#">Another action</a></li>
+                      <li><a className="dropdown-item" href="#">Something else here</a></li>
                     </ul>
-                  </div>
-                </div>
-                <div>
-                  <Link className="d-flex align-items-center gap-10 text-dark">
-                    <p className="mb-0">À propos</p>
-                  </Link>
-                </div>
-                <div>
-                  <Link className="d-flex align-items-center gap-10 text-dark">
-                    <p className="mb-0">Contact</p>
-                  </Link>
-                </div>
-                <div>
-                  <Link className="d-flex align-items-center gap-10 text-dark">
-                    <p className="mb-0">Compte</p>
-                  </Link>
-                </div>
-                <div>
-                  <Link className="d-flex align-items-center gap-10 text-dark">
-                    <img src="img/cart.svg" alt="Panier" />
-                    <div className="d-flex flex-column">
-                      <span className="badge bg-dark text-white">0</span>
-                      <p className="mb-0">50 €</p>
-                    </div>
-                  </Link>
-                </div>
+                  </li>
+                  <li>
+                    <Link className="d-flex align-items-center text-dark">
+                    À propos
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="d-flex align-items-center text-dark">
+                      Contact
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="d-flex align-items-center text-dark">
+                      Compte
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="d-flex align-items-center text-dark">
+                      <img src="img/cart.svg" alt="Panier" />
+                      <div className="d-flex flex-column">
+                        <span className="badge bg-dark text-white">0</span>
+                        <p className="mb-0">50 €</p>
+                      </div>
+                    </Link>
+                  </li>
+                </ul>
+                <span className="icon-menu">
+                  <LuMenu />
+                </span>
               </div>
             </div>
           </div>
         </div>
-      </header>
+      </header> */}
+
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid d-flex flex-nowrap justify-content-between">
+          <Link className="header-logo m-2 fs-1">
+            <img src="img/logo.svg" alt="Logo Kido'z" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="offcanvas offcanvas-end"
+            tabindex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-header gap-30">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                Explore le catalogue
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close">
+              </button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 fs-5 pe-3 gap-30">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false" >
+                    Menu
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    À propos
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contact
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                  Compte
+                  </a>
+                </li>
+              </ul>
+            </div>
+            </div>
+            <form className="search-bar" role="search">
+              <input className="form-control" type="search" placeholder="Rechercher un produit..." aria-label="Rechercher un produit..." />
+              <button className="btn btn-outline-success" type="submit">
+                <span className="input-group-text" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
+              </button>
+            </form>
+            <Link className="cart d-flex align-items-center text-dark">
+                <img src="img/cart.svg" alt="Panier" />
+                <div className="d-flex flex-column">
+                  <span className="badge bg-dark text-white">0</span>
+                  <p className="mb-0">50 €</p>
+                </div>
+              </Link>
+          </div>
+      </nav>
     </>
   );
 };
