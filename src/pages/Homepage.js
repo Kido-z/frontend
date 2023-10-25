6c8acc0ef3d26bb868f9cc21ca65676d5bf26dae
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2023-10-25 17:54:18 +0200

    [US-024] main banner for the homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,23 +1,74 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <>
+      <section className="banner-container">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="banner-image d-flex flex-column align-items-end justify-content-center pe-5">
+              <h1 class="display-4 fw-bold text-dark">Découvrez notre sélection</h1>
+              <div class="button-banner d-flex flex-row gap-3">
+                <div class="button-group me-2">
+                  <Link class="button-link btn-primary" style={{background: "#93BEE6"}}>
+                    De 0 à 1 an
+                  </Link>
+                </div>
+                <div class="button-group me-2">
+                  <Link class="button-link btn-secondary" style={{background: "#FDBF3A"}}>
+                  De 2 à 5 ans
+                  </Link>
+                </div>
+                <div class="button-group me-2">
+                  <Link class="button-link btn-third" style={{background: "#1FB36E"}}>
+                  À partir de 6 ans
+                  </Link>
+                </div>
+                {/* <div class="btn-group me-2" role="group" aria-label="Second group">
+                  <Link type="Link" class="btn btn-secondary">
+                    De 2 à 5 ans
+                  </Link>
+                </div>
+                <div class="btn-group" role="group" aria-label="Third group">
+                  <Link type="Link" class="btn btn-info">
+                    À partir de 6 ans
+                  </Link>
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </div>  
+        {/* <div className="col-6">
+              <div className="position-relative p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
+                <img src="img/banner.jpg" alt="banniere principal"/>
+              </div>
+            </div> 
 
-export default Homepage
+          <div class="position-relative overflow-hidden p-lg-5 text-center bg-body-tertiary">
+            <div class="col-md-6 p-lg-5 mx-auto my-5">
+              <img src="img/banner1.jpg" alt="banniere principal"/>
+              <h1 class="display-3 fw-bold">Designed for engineers</h1>
+              <h3 class="fw-normal text-muted mb-3">
+                Build anything you want with Aperture
+              </h3>
+              <div class="d-flex gap-3 justify-content-center lead fw-normal">
+                <a class="icon-link" href="#">
+                  Learn more
+                  <svg class="bi"></svg>
+                </a>
+                <a class="icon-link" href="#">
+                  Buy
+                  <svg class="bi"></svg>
+                </a>
+              </div>
+            </div>
+            <div class="product-device shadow-sm d-none d-md-block"></div>
+            <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+          </div> */}
+      </section>
+    </>
+  );
+};
 
-
-/* Je suis vivement intéressé par le poste de développeur chez Apsia, tel qu'annoncé.
-Avec une expérience solide en développement de logiciels et une passion pour la technologie,
-je suis convaincu que je peux apporter une contribution précieuse à votre entreprise.
-
-Mon expertise en [mentionnez des compétences pertinentes] et ma capacité à m'adapter rapidement
-aux nouvelles technologies font de moi un candidat idéal pour ce poste. J'admire l'engagement d'Apsia envers 
-l'innovation et je suis enthousiaste à l'idée de rejoindre votre équipe.
-
-Je suis convaincu que ma combinaison de compétences techniques et d'enthousiasme pour le développement logiciel
-sera un atout pour Apsia. Je suis impatient de discuter de ma contribution potentielle lors d'un entretien.
-
-Merci de considérer ma candidature. */
+export default Homepage;
