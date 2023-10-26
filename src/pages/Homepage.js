deff360a16849125118c42679fe74b40c163d9c1
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2023-10-26 11:14:13 +0200

    [US-024] section detail below the main banner

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -25,47 +25,24 @@ const Homepage = () => {
                   À partir de 6 ans
                   </Link>
                 </div>
-                {/* <div class="btn-group me-2" role="group" aria-label="Second group">
-                  <Link type="Link" class="btn btn-secondary">
-                    De 2 à 5 ans
-                  </Link>
-                </div>
-                <div class="btn-group" role="group" aria-label="Third group">
-                  <Link type="Link" class="btn btn-info">
-                    À partir de 6 ans
-                  </Link>
-                </div> */}
               </div>
             </div>
           </div>
-        </div>  
-        {/* <div className="col-6">
-              <div className="position-relative p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
-                <img src="img/banner.jpg" alt="banniere principal"/>
-              </div>
-            </div> 
-
-          <div class="position-relative overflow-hidden p-lg-5 text-center bg-body-tertiary">
-            <div class="col-md-6 p-lg-5 mx-auto my-5">
-              <img src="img/banner1.jpg" alt="banniere principal"/>
-              <h1 class="display-3 fw-bold">Designed for engineers</h1>
-              <h3 class="fw-normal text-muted mb-3">
-                Build anything you want with Aperture
-              </h3>
-              <div class="d-flex gap-3 justify-content-center lead fw-normal">
-                <a class="icon-link" href="#">
-                  Learn more
-                  <svg class="bi"></svg>
-                </a>
-                <a class="icon-link" href="#">
-                  Buy
-                  <svg class="bi"></svg>
-                </a>
-              </div>
+        </div>
+      </section>
+      <section className="container details">
+        <div className="row">
+          <h3 className="d-flex align-items-center justify-content-center fw-bold my-5">De supers jouets pour vos Kido’z</h3>
+            <div className="col-lg-4">
+              <p>Choisissez des jouets adaptés<br/> et éducatifs pour vos enfants </p>
             </div>
-            <div class="product-device shadow-sm d-none d-md-block"></div>
-            <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
-          </div> */}
+            <div className="col-lg-4">
+              <p>économisez chaque mois grâce<br/> à l’abonnement mensuel</p>
+            </div>
+            <div className="col-lg-4">
+              <p>Faites découvrir de nouveaux jouets<br/> tous les mois à vos enfants</p>
+            </div>
+        </div>
       </section>
     </>
   );
