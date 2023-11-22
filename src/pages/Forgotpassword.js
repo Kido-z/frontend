8520a0601c3fc55c726bdc52de58f4777745b62c
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2023-11-23 00:25:44 +0100

    [US-028] updated element of form

--- src/pages/Forgotpassword.js
+++ src/pages/Forgotpassword.js
@@ -9,14 +9,14 @@ const Forgetpassword = () => {
           <div className="col-lg-5 login-card">
             <h3 className="fw-bold d-flex justify-content-center">Mot de passe oublié</h3>
             <p className="d-flex justify-content-center">Nous vous ferons parvenir un e-mail pour réinitialiser votre mot de passe</p>
-            <form class="needs-validation" novalidate>
+            <form className="needs-validation" novalidate>
               <div className="py-5">
                 <div className="mb-3">
                   <input className="form-control" type="email" name="email" required placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
                 </div>
               </div>
               <div className="button-principal d-grid gap-2 col-4 mx-auto">
-                <button to="/" className="btn btn-primary mb-3" type="submit" style={{background: "#FDBF3A"}}>Valider</button>
+                <button className="btn btn-primary mb-3" type="submit" style={{background: "#FDBF3A"}}>Valider</button>
                 <Link to="/login" className="button-register" type="button">Annuler</Link>
               </div>
             </form>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,20 +8,20 @@ const Login = () => {
         <div className="row justify-content-md-center">
           <div className="col-lg-5 login-card">
             <h3 className="fw-bold d-flex justify-content-center">Connexion</h3>
-            <form class="needs-validation" novalidate>
+            <form className="needs-validation" novalidate>
               <div className="py-5">
                 <div className="mb-3">
                   <input className="form-control" type="email" name="email" required placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
                 </div>
                 <div className="mb-3">
-                  <input type="password" name="password" placeholder="Mot de passe" className="form-control"/>
+                  <input type="password" name="password" required placeholder="Mot de passe" className="form-control"/>
                 </div>
                 <div className="d-flex justify-content-end">
                   <Link to="/forgot-password" className="password">Mot de passe oublié ?</Link>
                 </div>
               </div>
               <div className="button-principal d-grid gap-2 col-4 mx-auto">
-                <button to="/" className="btn btn-primary mb-3" type="submit" style={{background: "#FDBF3A"}}>Se connecter</button>
+                <button className="btn btn-primary mb-3" type="submit" style={{background: "#FDBF3A"}}>Se connecter</button>
                 <Link to="/" className="button-register" type="button">Créer un compte</Link>
               </div>
             </form>
